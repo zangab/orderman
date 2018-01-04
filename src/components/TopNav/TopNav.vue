1ffd8e5d328d4d07faeaba1d30dc2f8cbb325d41
Gab Zan <gabriel.zangerl@towa.at>
2018-01-04 17:04:09 +0100

    feat: add TopNav component

---
+++ src/components/TopNav/TopNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="TopNav">
+    <router-link :to="{ name: 'login' }" class="left link">
+      <span class="fi flaticon-building"></span>
+    </router-link>
+
+    <h3 class="title">
+      Kirchtiman
+    </h3>
+
+    <router-link :to="{ name: 'login' }" class="right link">
+      <span class="fi flaticon-logout"></span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopNav',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import "../../styles/vars";
+  @import "./topnav";
+</style>
