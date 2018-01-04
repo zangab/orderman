d7369e81310bdbb09e7e20beddfb10f88efc82ad
Gab Zan <gabriel.zangerl@towa.at>
2018-01-04 17:03:54 +0100

    feat: add TabNav component

---
+++ src/components/TabNav/TabNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="TabNav">
+    <router-link :to="{ name: 'kellner-produkte' }" class="link">
+      <span class="fi flaticon-cooker"></span>
+    </router-link>
+    <router-link :to="{ name: 'login' }" class="link">
+      <span class="fi flaticon-archive"></span>
+    </router-link>
+    <router-link :to="{ name: 'login' }" class="link">
+      <span class="fi flaticon-trash"></span>
+    </router-link>
+    <router-link :to="{ name: 'login' }" class="link">
+      <span class="fi flaticon-right-arrow"></span>
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TabNav',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import "../../styles/vars";
+  @import "./tabnav";
+</style>
