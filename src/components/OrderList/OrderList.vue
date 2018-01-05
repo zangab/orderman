4cb2bd5971d686db583dbbafc6792c1d83f2bf49
Gab Zan <gabriel.zangerl@towa.at>
2018-01-05 15:28:22 +0100

    feat: add OrderList component

---
+++ src/components/OrderList/OrderList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="OrderList">
+    Order
+    <!-- <product-list-item v-for="(item, index) in data" :key="item.id" :item="item"></product-list-item> -->
+  </ul>
+</template>
+
+<script>
+import { list } from './data'
+
+export default {
+  name: 'OrderList',
+  data () {
+    return {
+      data: list
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../styles/vars";
+  @import "./list";
+</style>
---
+++ src/components/OrderList/data.js
@@ -0,0 +1,4 @@
+export default {}
+
+const list = []
+export { list }
