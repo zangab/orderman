1bbf831454be192fe24a71b3b01ecf54e0c1549b
Gab Zan <gabriel.zangerl@towa.at>
2018-01-05 15:30:53 +0100

    chore: udpate core stuff

---
+++ src/locale/index.js
@@ -0,0 +1,39 @@
+/**
+ * Simplified version
+ * https://github.com/ElemeFE/element/blob/dev/src/locale/index.js
+ */
+import Vue from 'vue'
+import defaultLang from './lang/at-DE'
+
+const i18nHandler = function (...args) {
+  const vuei18n = Object.getPrototypeOf(this || Vue).$t
+
+  if (typeof vuei18n === 'function') {
+    return vuei18n.apply(this, args)
+  }
+}
+
+export function t (...args) {
+  const array = args[0].split('.')
+  let value = i18nHandler.apply(this, args)
+  let current = defaultLang
+
+  if (value !== null && typeof value !== 'undefined') {
+    return value
+  }
+
+  for (let i = 0, len = array.length; i < len; i++) {
+    const property = array[i]
+    value = current[property]
+
+    if (i === len - 1) {
+      return value
+    } else if (!value) {
+      return ''
+    }
+
+    current = value
+  }
+
+  return ''
+}
---
+++ src/locale/lang/at-DE.js
@@ -0,0 +1,27 @@
+export default {
+  at: {
+    select: {
+      placeholder: 'Auswählen',
+      notFoundText: 'Keine Daten gefunden'
+    },
+    modal: {
+      okText: 'OK',
+      cancelText: 'Abbrechen'
+    },
+    pagination: {
+      prevText: 'Vorherige Seite',
+      nextText: 'Nächste Seite',
+      total: 'Total',
+      item: 'Item',
+      items: 'Items',
+      pageSize: '/ Seite',
+      goto: 'Gehe zu',
+      pageText: '',
+      prev5Text: 'Vorherigen 5 Seiten',
+      next5Text: 'Nächsten 5 Seiten'
+    },
+    table: {
+      emptyText: 'No data'
+    }
+  }
+}
