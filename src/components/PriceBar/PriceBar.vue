99d59013152575aab0d71966b1c71cc5659f71d6
Gab Zan <gabriel.zangerl@towa.at>
2018-01-05 15:27:59 +0100

    feat: add PriceBar component

---
+++ src/components/PriceBar/PriceBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="PriceBar">
+    <strong>€ 14.50</strong>
+    <small>Bestellsumme</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PriceBar',
+  data () {
+    return {
+      data: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../styles/vars";
+  @import "./price";
+</style>
