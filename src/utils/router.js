21c71ed8fadb5ed53e0dd9717762185d375f2b02
Gab Zan <gabriel.zangerl@towa.at>
2018-01-05 15:29:32 +0100

    chore: update routes

--- src/utils/router.js
+++ src/utils/router.js
@@ -2,16 +2,41 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Login from '@/components/Templates/Login'
-// import Home from '@/components/Templates/Home'
+import Home from '@/components/Templates/Home'
+import ProductList from '@/components/ProductList/List'
+import OrderList from '@/components/OrderList/OrderList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Login',
+      name: 'login',
       component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/kellner',
+      name: 'kellner-home',
+      component: Home,
+      children: [
+        {
+          path: 'produkte',
+          name: 'kellner-produkte',
+          component: ProductList
+        },
+        {
+          path: 'archiv',
+          name: 'kellner-archiv',
+          component: OrderList
+        }
+      ]
     }
   ]
 })
