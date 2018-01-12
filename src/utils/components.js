698e13a7daeb84ce15ee932f6ff3911a7ae0db13
Gab Zan <gabriel.zangerl@towa.at>
2018-01-12 17:03:28 +0100

    feat: update mockdata and component registration

--- src/utils/components.js
+++ src/utils/components.js
@@ -3,12 +3,18 @@ import Vue from 'vue'
 import TopNav from '@/components/TopNav/TopNav'
 import TabNav from '@/components/TabNav/TabNav'
 import ProductListItem from '@/components/ProductListItem/Item'
+import OrderList from '@/components/OrderList/OrderList'
+import OrderListItem from '@/components/OrderListItem/Item'
 import PriceBar from '@/components/PriceBar/PriceBar'
+import Cashout from '@/components/Cashout/Cashout'
 
 Vue.component('topNav', TopNav)
 Vue.component('tabNav', TabNav)
 Vue.component('productListItem', ProductListItem)
+Vue.component('orderList', OrderList)
+Vue.component('orderListItem', OrderListItem)
 Vue.component('priceBar', PriceBar)
+Vue.component('cashOut', Cashout)
 
 Vue.filter('currency', (value) => {
   return value.toFixed(2)
--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -1,6 +1,129 @@
 export default {}
 
-const drinks = [
+/* eslint-disable no-unused-vars */
+const drinks = {
+  1: {
+    id: 1,
+    img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTl_6cBGYiU7_X82DSSVCr0UBkY14piF4kuqQpOLDS3R_sz-DmA',
+    name: 'Bier 0,5l',
+    price: 3.0,
+    category: 'drink'
+  },
+  2: {
+    id: 2,
+    img: 'http://www.bitburger-international.com/fileadmin/user_upload/bitburger-international/initialer-Content/P_9.0_Wernesgruener/P_5.2_T_3_Produkte_Wernesgruener/50121_WGR_Bildmarke_Radler_440x260px.jpg',
+    name: 'Radler 0,5l',
+    price: 3.0,
+    category: 'drink'
+  },
+  3: {
+    id: 3,
+    img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTl_6cBGYiU7_X82DSSVCr0UBkY14piF4kuqQpOLDS3R_sz-DmA',
+    name: 'Bier 0,3l',
+    price: 2.0,
+    category: 'drink'
+  },
+  4: {
+    id: 4,
+    img: 'http://www.bitburger-international.com/fileadmin/user_upload/bitburger-international/initialer-Content/P_9.0_Wernesgruener/P_5.2_T_3_Produkte_Wernesgruener/50121_WGR_Bildmarke_Radler_440x260px.jpg',
+    name: 'Radler 0,3l',
+    price: 2.0,
+    category: 'drink'
+  },
+  5: {
+    id: 5,
+    img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTXbHo9F7dizIhjyPFp98cTP6CFic0Fm9iyMxxxLobknr3PpaGggw',
+    name: 'Cola',
+    price: 2.0,
+    category: 'drink'
+  },
+  6: {
+    id: 6,
+    img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSI22QRMXyL3Elx0lIFoQe4K6yjx0QWTmILfDfLkwgiwxi1GFro',
+    name: 'Fanta',
+    price: 2.0,
+    category: 'drink'
+  },
+  7: {
+    id: 7,
+    img: 'http://vignette3.wikia.nocookie.net/logopedia/images/f/f5/Sprite_logo2.jpg/revision/latest?cb=20140618132523',
+    name: 'Sprite',
+    price: 2.0,
+    category: 'drink'
+  },
+  8: {
+    id: 8,
+    img: 'http://2.bp.blogspot.com/_jhiaT6dZgx0/S4yYuWaqZ1I/AAAAAAAACrU/2e_GbdX8dgw/s320/Almdudler+Sign1.jpg',
+    name: 'Almdudler',
+    price: 2.0,
+    category: 'drink'
+  },
+  9: {
+    id: 9,
+    img: 'http://www.bitstrom.at/tl_files/_Uploads/portfolio/kju/2015/roemerquelle/rqweb2015_thumb_s02.jpg',
+    name: 'Mineral',
+    price: 2.0,
+    category: 'drink'
+  },
+  10: {
+    id: 10,
+    img: 'http://de.stockfresh.com/thumbs/stokkete/4033847_Weinprobe-Restaurant-Flasche-Glas-Rotwein-Party.jpg',
+    name: 'Wein Rot 0,25l',
+    price: 3.8,
+    category: 'drink'
+  },
+  11: {
+    id: 11,
+    img: 'https://www.kochland.de/imageshrkat/rieslingglaeser.jpg',
+    name: 'Wein Weiß 0,25l',
+    price: 3.8,
+    category: 'drink'
+  },
+  12: {
+    id: 12,
+    img: 'http://de.stockfresh.com/thumbs/stokkete/4033847_Weinprobe-Restaurant-Flasche-Glas-Rotwein-Party.jpg',
+    name: 'Gspritzter Rot Süß',
+    price: 2.5,
+    category: 'drink'
+  },
+  13: {
+    id: 13,
+    img: 'https://www.kochland.de/imageshrkat/rieslingglaeser.jpg',
+    name: 'Gspritzter Rot Sauer',
+    price: 2.5,
+    category: 'drink'
+  },
+  14: {
+    id: 14,
+    img: 'https://wienerweinguide.files.wordpress.com/2013/06/gspritzter_v_c3b6wm.jpg',
+    name: 'Gspritzter Weiß Süß',
+    price: 2.5,
+    category: 'drink'
+  },
+  15: {
+    id: 15,
+    img: 'https://wienerweinguide.files.wordpress.com/2013/06/gspritzter_v_c3b6wm.jpg',
+    name: 'Gspritzter Weiß Sauer',
+    price: 2.5,
+    category: 'drink'
+  },
+  16: {
+    id: 16,
+    img: 'http://www.schmidee.com/wp-content/uploads/2014/10/kaffee-schmidee.jpg',
+    name: 'Kaffee',
+    price: 2.0,
+    category: 'drink'
+  },
+  17: {
+    id: 17,
+    img: 'https://www.gutekueche.at/img/rezept/18365/300x199_birnenschnaps-.jpg',
+    name: 'Schnaps',
+    price: 2.5,
+    category: 'drink'
+  }
+}
+
+const drink = [
   {
     id: 1,
     img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTl_6cBGYiU7_X82DSSVCr0UBkY14piF4kuqQpOLDS3R_sz-DmA',
@@ -122,7 +245,45 @@ const drinks = [
   }
 ]
 
-const food = [
+const food = {
+  18: {
+    id: 18,
+    img: 'http://images.ichkoche.at/data/image/variations/496x384/2/pommes-frites-rezept-img-19041.jpg',
+    name: 'Pommes',
+    price: 2.5,
+    category: 'food'
+  },
+  19: {
+    id: 19,
+    img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSNkk1w5oc5diVPR_Oq9ZzB7nFOtg4a11MkOYRo7ZDi-PlBxiqSug',
+    name: 'Grillwurst',
+    price: 3.5,
+    category: 'food'
+  },
+  20: {
+    id: 20,
+    img: 'https://blogjob.com/whisk/wp-content/blogs.dir/2072/files/2015/12/Chicken-Schnitzel-e1450616486763.png',
+    name: 'Schnitzelburger',
+    price: 5.0,
+    category: 'food'
+  },
+  21: {
+    id: 21,
+    img: 'http://www.gasthof-lang.de/wp-content/gallery/blick-in-die-kueche/schnitzel.jpg',
+    name: 'Schnitzel Pommes',
+    price: 8.0,
+    category: 'food'
+  },
+  22: {
+    id: 22,
+    img: 'http://www.genusslandsuedtirol.it/i/recipe/i50_1.jpg',
+    name: 'Schnitzel Kartoffelsalat',
+    price: 8.0,
+    category: 'food'
+  }
+}
+
+const foods = [
   {
     id: 16,
     img: 'http://images.ichkoche.at/data/image/variations/496x384/2/pommes-frites-rezept-img-19041.jpg',
