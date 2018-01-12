b1742a6db914221ed4d779704c3c179019387965
Gab Zan <gabriel.zangerl@towa.at>
2018-01-12 17:01:43 +0100

    feat: add Cashout component

---
+++ src/components/Cashout/Cashout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="Cashout">
+    <h3>Bestellung abschließen</h3>
+    <div class="list">
+      <div class="child">
+        <label>Tischnummer</label>
+        <input v-model="tableNum" placeholder="0">
+      </div>
+      <div class="child">
+        <label>&nbsp;</label>
+        <button class="button">OK</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Order from '../../utils/order'
+import EventBus from '../../utils/eventbus'
+
+export default {
+  name: 'OrderList',
+  mounted () {
+    EventBus.$on('resetOrder', () => {
+      this.products = this.$set(this, 'products', Order.getProducts())
+      this.$router.push({ name: 'kellner-produkte' })
+    })
+  },
+  data () {
+    return {
+      tableNum: 0
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../styles/vars";
+  @import "./cashout";
+</style>
