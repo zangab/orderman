d63a39ed98db2cd58ddff29f071c9f8bc80f04f7
Gab Zan <gabriel.zangerl@towa.at>
2018-01-22 21:41:24 +0100

    feat: register new components, filters and global scss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './utils/router'
+import VueSweetalert2 from 'vue-sweetalert2'
 import './utils/components'
 
+Vue.use(VueSweetalert2)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
