e64eea0ea59b51d3d0b9c4a9f15fc8cdaba92064
Gab Zan <gabriel.zangerl@towa.at>
2018-01-22 21:38:41 +0100

    feat: add add tableNum, button and sweetalert to cashout

--- src/components/Cashout/Cashout.vue
+++ src/components/Cashout/Cashout.vue
@@ -4,11 +4,13 @@
     <div class="list">
       <div class="child">
         <label>Tischnummer</label>
-        <input v-model="tableNum" placeholder="0">
+        <!-- <v-select v-model="tableNum" :options="options" :class="['table-select', { 'has-error': tableNumError }]" id="selection"></v-select> -->
+        <input type="text" @click="openSelect">
       </div>
       <div class="child">
-        <label>&nbsp;</label>
-        <button class="button">OK</button>
+        <button class="button" @click="checkout">
+          <i class="fi flaticon-tick"></i>
+        </button>
       </div>
     </div>
   </div>
@@ -28,10 +30,68 @@ export default {
   },
   data () {
     return {
-      tableNum: 0
+      tableNum: '101',
+      tableNumError: false,
+      options: [
+        '100', '101', '102', '103', '104', '105',
+        '200', '201', '202', '203', '204', '205',
+        '300', '301', '302', '303', '304', '305'
+      ]
     }
   },
   methods: {
+    checkout () {
+      if (this.tableNum !== null) {
+        this.tableNumError = false
+        this.$swal({
+          title: 'Bestellung abschicken?',
+          text: 'Ist die Bestellung auf Tischnummer #' + this.tableNum + ' soweit richtig?',
+          type: 'question',
+          showCancelButton: true,
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Abbrechen',
+          confirmButtonColor: '#03B4D9',
+          showLoaderOnConfirm: true,
+          allowOutsideClick: () => !this.$swal.isLoading(),
+          preConfirm: () => {
+            return new Promise((resolve) => {
+              // TODO: api request
+            })
+          }
+        }).then((res) => {
+          if (res.value) {
+            this.$swal(
+              'Erfolgreich',
+              'Die Bestellung wurde erfolgreich abgeschickt!',
+              'success'
+            )
+          }
+        })
+      } else {
+        // is null
+        this.tableNumError = true
+      }
+    },
+    openSelect () {
+      this.$swal({
+        title: 'Tischnummer',
+        input: 'select',
+        inputOptions: {
+          '101': '101',
+          '201': '201',
+          '301': '301'
+        },
+        type: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Abbrechen',
+        confirmButtonColor: '#03B4D9'
+      }).then((res) => {
+        if (res.value) {
+
+        }
+      })
+    }
   }
 }
 </script>
