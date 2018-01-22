2c05860ec468b319c45f59b3ea721a0f691e5641
Gab Zan <gabriel.zangerl@towa.at>
2018-01-22 21:36:54 +0100

    feat: add ArchiveList component

---
+++ src/components/ArchiveList/ArchiveList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="ArchiveList">
+    <li class="sectiontitle" :style="{'position': 'fixed', 'top': '71px'}">Alle Bestellungen</li>
+    <archive-list-item
+      v-for="item in history"
+      :key="item.id"
+      :item="item"
+      track-by="item.id"
+    ></archive-list-item>
+  </ul>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+import Order from '../../utils/order'
+import { history } from '../../utils/mockdata'
+
+export default {
+  name: 'ArchiveList',
+  data () {
+    return {
+      history
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../styles/vars";
+  @import "./list";
+</style>
